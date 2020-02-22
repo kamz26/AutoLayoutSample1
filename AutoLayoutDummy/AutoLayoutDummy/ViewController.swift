b919e589255df76a2d7678a523c3a33fc2769df0
Abhisek Kasyap <abhisek.kasyap@capillarytech.com>
2020-02-22 17:45:07 +0530

    first commit

---
+++ AutoLayoutDummy/AutoLayoutDummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDummy
+//
+//  Created by Abhisek K. on 22/02/20.
+//  Copyright © 2020 Abhisek K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
